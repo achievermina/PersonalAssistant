86910483466434f9962d33673c4c9e5b1ffbd2d4
achievermina <lmina820@gmail.com>
2020-05-29 15:51:35 -0700

    Update github for deleted files

--- init-product.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Creating table football-results \n"
-
-AWS_DEFAULT_REGION=us-east-1  # 코드가 실행될떄 기본값을 불러들이는데 환경변수에서 가져옴
-
-aws dynamodb create-table --table-name user-info \
-  --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
-
